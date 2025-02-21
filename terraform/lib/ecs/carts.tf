33df5724a02433a4b9474d774dbe50cd73c3516d
am2308 <b110020510@gmail.com>
2025-02-21 17:33:13 +0000

    feat:added new repo

--- terraform/lib/ecs/carts.tf
+++ terraform/lib/ecs/carts.tf
@@ -5,9 +5,9 @@ module "carts_service" {
   service_name                    = "carts"
   cluster_arn                     = aws_ecs_cluster.cluster.arn
   vpc_id                          = var.vpc_id
-  vpc_cidr                        = var.vpc_cidr
+  #vpc_cidr                        = var.vpc_cidr
   subnet_ids                      = var.subnet_ids
-  public_subnet_ids               = var.public_subnet_ids
+  #public_subnet_ids               = var.public_subnet_ids
   tags                            = var.tags
   container_image                 = module.container_images.result.cart.url
   service_discovery_namespace_arn = aws_service_discovery_private_dns_namespace.this.arn
