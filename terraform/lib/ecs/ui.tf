64af1dbe8b799d2006fee90e37551f9fee903bb4
am2308 <b110020510@gmail.com>
2025-02-21 18:47:42 +0000

    feat:added new repo

--- terraform/lib/ecs/ui.tf
+++ terraform/lib/ecs/ui.tf
@@ -5,9 +5,9 @@ module "ui_service" {
   service_name                    = "ui"
   cluster_arn                     = aws_ecs_cluster.cluster.arn
   vpc_id                          = var.vpc_id
-  vpc_cidr                        = var.vpc_cidr
+  #vpc_cidr                        = var.vpc_cidr
   subnet_ids                      = var.subnet_ids
-  public_subnet_ids               = var.public_subnet_ids
+  #public_subnet_ids               = var.public_subnet_ids
   tags                            = var.tags
   container_image                 = module.container_images.result.ui.url
   service_discovery_namespace_arn = aws_service_discovery_private_dns_namespace.this.arn
