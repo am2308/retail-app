763bbe7268e817957def574a68f04084a296effd
am2308 <b110020510@gmail.com>
2025-07-05 12:42:12 +0100

    Update mq.tf

--- terraform/lib/dependencies/mq.tf
+++ terraform/lib/dependencies/mq.tf
@@ -15,6 +15,7 @@ resource "aws_mq_broker" "mq" {
   engine_type         = "RabbitMQ"
   #engine_version      = "3.11.28"
   engine_version      = "3.13"
+  auto_minor_version_upgrade = true
   host_instance_type  = "mq.t3.micro"
   deployment_mode     = "SINGLE_INSTANCE"
   subnet_ids          = [var.subnet_ids[0]]
