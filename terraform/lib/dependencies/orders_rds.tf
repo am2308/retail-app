785b9d1b53dde2ba55cf5d165d5eb07268b9f137
am2308 <b110020510@gmail.com>
2026-06-29 21:15:33 +0100

    Update orders_rds.tf

--- terraform/lib/dependencies/orders_rds.tf
+++ terraform/lib/dependencies/orders_rds.tf
@@ -4,7 +4,7 @@ module "orders_rds" {
 
   name           = "${var.environment_name}-orders"
   engine         = "aurora-postgresql"
-  engine_version = "16.1"
+  engine_version = "16.4"
   engine_mode    = "provisioned"
   instance_class = "db.t3.medium"
 
